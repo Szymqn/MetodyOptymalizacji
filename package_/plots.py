9a04181f47b40fa290a9016424f4b28d8ed06294
szymqn <koodziejskisz@outlook.com>
2024-04-15 14:12:01 +0200

    Added 3d plot

--- package_/plots.py
+++ package_/plots.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+
 
 def plot_one_variable_with_range_and_middle(A, B, x_sr, x_1, x_2, f):
     x = np.linspace(A, B, 1000)
@@ -32,5 +35,28 @@ def plot_one_variable_with_range_one_points(A, B, f, point):
     y = f(x)
     plt.plot(x, y)
     plt.scatter(point, f(point), color='green', label='point')
+    plt.legend()
+    plt.show()
+
+
+def plot_two_variables_with_start_point(x, y, f):
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    Z = f(X, Y)
+
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False, alpha=0.5)
+
+    ax.set_zlim(np.min(Z), np.max(Z))
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter('{x:.02f}')
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    ax.scatter(x, y, f(x, y), color='yellow', label='point')
+    print(x, y, f(x, y))
+
     plt.legend()
     plt.show()
